b55311503429456c4da1a208b076feb3bc04b703
Jose Oviedo <josepolvoraoviedo@gmail.com>
2024-09-02 21:43:13 -0300

    Actualizar .gitignore y eliminar node_modules del índice

---
+++ src/router/index.router.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+
+// Todas las RUTAS
+
+const carritoRouter = require("./carrito.router");
+router.use("/", carritoRouter);
+
+const carritoxproductoRouter = require("./carritoxproducto.router");
+router.use("/", carritoxproductoRouter);
+
+const categoriaRouter = require("./categoria.router");
+router.use("/", categoriaRouter);
+
+const clienteRouter = require("./cliente.router");
+router.use("/", clienteRouter);
+
+const domicilioRouter = require("./domicilio.router");
+router.use("/", domicilioRouter);
+
+const envioRouter = require("./envio.router");
+router.use("/", envioRouter);
+
+const imagenRouter = require("./imagen.router");
+router.use("/", imagenRouter);
+
+const localidadRouter = require("./localidad.router");
+router.use("/", localidadRouter);
+
+const productoRouter = require("./producto.router");
+router.use("/", productoRouter);
+
+const productoxcategoriaRouter = require("./productoxcategoria.router");
+router.use("/", productoxcategoriaRouter);
+
+const provinciaRouter = require("./provincia.router");
+router.use("/", provinciaRouter);
+
+const usuarioRouter = require("./usuario.router");
+router.use("/", usuarioRouter);
+
+const ventaRouter = require("./venta.router");
+router.use("/", ventaRouter);
+
+module.exports = router;
